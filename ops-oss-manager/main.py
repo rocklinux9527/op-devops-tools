5196c8296ba10ca4995b4807502399637a8d115b
rocklinux9527 <120091528+rocklinux9527@users.noreply.github.com>
2023-08-03 16:16:04 +0800

    Update  download main.py

--- ops-oss-manager/main.py
+++ ops-oss-manager/main.py
@@ -41,7 +41,6 @@ class AliYunOssManager():
         self.pack_output_path = YamlHandler().get("pack_output_path")
         self.delete_file_path = YamlHandler().get("delete_file_path")
         self.list_file_path = YamlHandler().get("list_file_path")
-        self.download_bucket_dir = YamlHandler().get("download_bucket_dir")
         self.download_local_dir = YamlHandler().get("download_local_dir")
 
     def upload_file_oss(self):
@@ -51,8 +50,8 @@ class AliYunOssManager():
                     local_path = os.path.join(root, file)
                     oss_path = os.path.join(self.upload_bucket_dir,
                                             os.path.relpath(local_path, self.upload_local_dir).replace("\\", "/"))
-                    print("oss路径",oss_path)
-                    print("本地路径",local_path)
+                    print("oss路径", oss_path)
+                    print("本地路径", local_path)
                     self.bucket.put_object_from_file(oss_path, local_path)
                     logger.info(f"Uploaded {local_path} to {oss_path}")
             print("Uploaded file Success")
@@ -71,62 +70,26 @@ class AliYunOssManager():
             logger.error(f"Failed to list OSS files: {str(e)}")
 
     def prefix_all_list(self, prefix):
-        print("开始列举"+prefix+"全部文件");
+        print("开始列举" + prefix + "全部文件");
         oss_file_size = 0;
-        for obj in oss2.ObjectIterator(self.bucket,prefix='%s/'%prefix):
+        for obj in oss2.ObjectIterator(self.bucket, prefix='%s/' % prefix):
             print(' key : ' + obj.key)
             oss_file_size = oss_file_size + 1;
             self.download_to_local(obj.key, obj.key)
-        print(prefix +" file size " + str(oss_file_size));
+        print(prefix + " file size " + str(oss_file_size));
 
-    def root_directory_list(self):
-        # 设置Delimiter参数为正斜线（/）。
-        for obj in oss2.ObjectIterator(self.bucket, prefix =self.download_bucket_dir, delimiter='/'):
-            # 通过is_prefix方法判断obj是否为文件夹。
-            if obj.is_prefix():  # 文件夹
-                print('directory: ' + obj.key);
-                self.prefix_all_list(str(obj.key).strip("/")); #去除/
-            else:  # 文件
-                print('file: ' + obj.key);
-                #下载根目录的单个文件
-                self.download_to_local(str(obj.key) , str(obj.key));
-
-    def download_to_local(self,object_name,local_file):
-        download_local_save_prefix = self.download_local_dir
-        url = download_local_save_prefix + local_file;
-        #文件名称
-        file_name = url[url.rindex("/")+1:]
-        file_path_prefix = url.replace(file_name, "")
-        if False == os.path.exists(file_path_prefix):
-            os.makedirs(file_path_prefix);
-            print("directory don't not make dirs "+  file_path_prefix);
-            # 下载OSS文件到本地文件。如果指定的本地文件存在会覆盖，不存在则新建。
-            self.bucket.get_object_to_file(object_name, download_local_save_prefix+local_file)
-
-    def download_from_oss(self):
-        self._download_directory(self.download_bucket_dir,self.download_local_dir)
-
-    def _download_directory(self, oss_dir, save_dir):
-        stack = [(oss_dir, save_dir)]  # 使用栈来实现迭代
-        while stack:
-            current_oss_dir, current_save_dir = stack.pop()
-            for obj in oss2.ObjectIterator(self.bucket, prefix=current_oss_dir):
-                if obj.key == current_oss_dir:  # 如果是当前目录，则跳过
-                    print("这是目录",current_oss_dir)
+    def download_objects_in_directory(self,download_bucket_dir):
+        files = list(oss2.ObjectIterator(self.bucket, prefix=download_bucket_dir, delimiter='/'))
+        for obj in files:
+            if obj.is_prefix():  # 如果是目录，则递归下载子目录
+                os.makedirs(self.download_local_dir + obj.key, exist_ok=True)
+                self.download_objects_in_directory(obj.key)
+            else:
+                # 下载对象到本地文件
+                if obj.key == download_bucket_dir:
                     continue
-                if obj.key.startswith(current_oss_dir):  # 如果是子目录或文件，则处理
-                    relative_path = obj.key.lstrip('/')
-                    if obj.key.endswith('/'):  # 如果是目录，则创建目录
-                        sub_save_dir = os.path.join(current_save_dir, relative_path)
-                        os.makedirs(sub_save_dir, exist_ok=True)
-                        stack.append((obj.key, sub_save_dir))
-                    else:  # 如果是文件，则下载到本地目录
-                        file_name = os.path.basename(obj.key)
-                        print("****",current_save_dir)
-                        save_path = os.path.join(current_save_dir,relative_path,file_name)
-                        os.makedirs(os.path.dirname(save_path), exist_ok=True)  # 先创建父级目录
-                        self.bucket.get_object_to_file(obj.key, save_path)
-                        print(f"Downloaded '{obj.key}' to '{save_path}'.")
+                self.bucket.get_object_to_file(obj.key, self.download_local_dir + obj.key)
+                print(f'Downloaded: {obj.key} -> {self.download_local_dir}')
 
     def delete_file_oss(self):
         prefix = self.delete_file_path
@@ -172,10 +135,8 @@ if __name__ == "__main__":
     elif action == "list":
         oss_manager.list_file_oss()
     elif action == "download":
-       # oss_manager.download_from_oss()
-       oss_manager.root_directory_list()
-       print("end \n")
-
+        oss_manager.download_objects_in_directory(YamlHandler().get("download_bucket_dir"))
+        print("end \n")
     elif action == "delete":
         oss_manager.delete_file_oss()
     elif action == "pack":
